f5d97f7b4b4e209a74ce94befcf4eab9c26005c6
MaleckBm28 <maleck.abm28@gmail.com>
2025-10-16 13:35:46 +0200

    clases

---
+++ reto1crud/src/principal/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+
+import javax.security.auth.login.LoginException;
+import utilidades.Util;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Principal {
+    public static void main(String[] args) throws LoginException{
+        
+        
+        
+    }
+}
