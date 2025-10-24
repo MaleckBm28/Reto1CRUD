4344a0e9e4b2985d1c4484c051b8142a5dff8dbf
ximenaqz <jimenaquiroz.1510@gmail.com>
2025-10-24 10:45:12 +0200

    pool carpetas
    primer creacion del pool

--- reto1crud/src/principal/Principal.java
+++ reto1crud/src/principal/Principal.java
@@ -5,18 +5,23 @@
  */
 package principal;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import javax.security.auth.login.LoginException;
-import utilidades.Util;
+public class Principal extends Application {
 
-/**
- *
- * @author 2dam
- */
-public class Principal {
-    public static void main(String[] args) throws LoginException{
-        
-        
-        
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/FXMLDocument.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Prueba FXML");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
